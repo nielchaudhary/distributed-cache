da7a9d036c6b52a9b442eff763339419cceb6079
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-02-20 23:50:40 +0530

    set up an rpc sv

--- internal/rpc/rpc.go
+++ internal/rpc/rpc.go
@@ -1,5 +1,15 @@
 package rpc
 
+import (
+	"net"
+	"net/rpc"
+)
+
+type Cache interface {
+	Get(key string) (interface{}, bool)
+	Put(key string, value string)
+}
+
 type CacheService struct {
 	cache Cache
 }
@@ -17,4 +27,23 @@ func (c *CacheService) Put(args [2]string, reply *bool) error {
 	c.cache.Put(args[0], args[1])
 	*reply = true
 	return nil
+}
+
+func startServer(cache Cache, address string) error {
+	service := &CacheService{cache: cache}
+	rpc.Register(service)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+
+		go rpc.ServeConn(conn)
+	}
+
 }
